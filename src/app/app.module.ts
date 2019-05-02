52d79ba00500ecfd611c04adee136831cad18d10
tranquangkhai <tranquangkhaiiq@yahoo.com>
2019-05-02 16:26:46 +0700

    create header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    // HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { TrangChuComponent } from './trang-chu/trang-chu.component';
-
+import { HomeComponent } from './home.component';
+import { from } from 'rxjs';
+import { HeaderComponent } from './trang-chu/header/header.component';
 @NgModule({
-  declarations: [TrangChuComponent],
+  declarations: [HomeComponent, TrangChuComponent, HeaderComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
