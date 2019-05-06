d3ecdb30131d76ced6a10890924ab817701662ff
tranquangkhai <tranquangkhaiiq@yahoo.com>
2019-05-06 18:31:44 +0700

    06052019-3

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -9,9 +9,10 @@ import { CarouselComponent } from './trang-chu/carousel/carousel.component';
 import { OwlModule } from 'ngx-owl-carousel';
 import { ContentTabComponent } from './trang-chu/content-tab/content-tab.component';
 import { SharedModule } from '../shared/shared.module';
+import { FooterComponent } from './trang-chu/footer/footer.component';
 
 @NgModule({
-  declarations: [HomeComponent, TrangChuComponent, HeaderComponent, CarouselComponent, ContentTabComponent],
+  declarations: [HomeComponent, TrangChuComponent, HeaderComponent, CarouselComponent, ContentTabComponent, FooterComponent],
   imports: [
     CommonModule,
     HomeRoutingModule, OwlModule, SharedModule
--- src/app/home/trang-chu/carousel/carousel.component.ts
+++ src/app/home/trang-chu/carousel/carousel.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { QuanlyphimService } from 'src/app/_cores/services/quanlyphim.service';
+import { Subscription} from 'rxjs';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
-
-  constructor() { }
+export class CarouselComponent implements OnInit, OnDestroy {
+  subService: Subscription;
+  dsPhim: any[] = [];
+  // myCarouselImages: any[] = [];
+  constructor(private qlyPhim: QuanlyphimService) { }
   // This is to generate random placeholder images
   // tslint:disable-next-line:member-ordering
   // mySlideImages = [1, 2, 3].map((i) => `https://picsum.photos/640/480?image=${i}`);
@@ -18,6 +21,20 @@ export class CarouselComponent implements OnInit {
   myCarouselOptions = {items: 3, dots: true, nav: true, loop: true};
 
   ngOnInit() {
+    this.subService = this.qlyPhim.LayDanhSachPhim()
+      .subscribe((ketqua) => {
+        this.dsPhim = ketqua;
+        // for (const phim of this.dsPhim) {
+        //   if (this.myCarouselImages.length < 6) {
+        //     this.myCarouselImages.push( phim.HinhAnh );
+        //   }
+        // }
+        // this.myCarouselOptions = {items: 3, dots: true, nav: true, loop: true};
+      }, (loi) => {
+        console.log(loi);
+      });
   }
-
+  ngOnDestroy() {
+    this.subService.unsubscribe(); // Hủy theo dõi biến observable
+  }
 }
