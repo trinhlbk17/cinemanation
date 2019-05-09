b588b39fbb845fe4dde2a2ff15184070c03ce215
tranquangkhai <tranquangkhaiiq@yahoo.com>
2019-05-09 14:44:06 +0700

    update_img

--- src/app/home/trang-chu/carousel/carousel.component.ts
+++ src/app/home/trang-chu/carousel/carousel.component.ts
@@ -15,7 +15,7 @@ export class CarouselComponent implements OnInit, OnDestroy {
   // tslint:disable-next-line:member-ordering
   // mySlideImages = [1, 2, 3].map((i) => `https://picsum.photos/640/480?image=${i}`);
   // tslint:disable-next-line:member-ordering
-  myCarouselImages = [1, 2, 3, 4, 5, 6].map((i) => `https://picsum.photos/640/480?image=${i}`);
+  myCarouselImages = [1, 2, 3, 4, 5, 6].map((i) => `./assets/img/${i}.jpg`);
   // tslint:disable-next-line:member-ordering
   // mySlideOptions = {items: 1, dots: true, nav: false};
   myCarouselOptions = {items: 3, dots: true, nav: true, loop: true};
