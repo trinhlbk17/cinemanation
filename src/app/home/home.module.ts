525a80d6f64a3308465bc07146cfb0aa109f841f
tranquangkhai <tranquangkhaiiq@yahoo.com>
2019-05-13 15:29:15 +0700

    13052019

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -12,9 +12,12 @@ import { SharedModule } from '../shared/shared.module';
 import { FooterComponent } from './trang-chu/footer/footer.component';
 import { DetailMovieComponent } from './trang-chu/detail-movie/detail-movie.component';
 import { TimeOfMovieComponent } from './trang-chu/detail-movie/time-of-movie/time-of-movie.component';
+import { OrderTicketsComponent } from './trang-chu/detail-movie/time-of-movie/order-tickets/order-tickets.component';
+import { DanhSachGheComponent } from './trang-chu/detail-movie/time-of-movie/danh-sach-ghe/danh-sach-ghe.component';
+import { GheComponent } from './trang-chu/detail-movie/time-of-movie/danh-sach-ghe/ghe/ghe.component';
 
 @NgModule({
-  declarations: [HomeComponent, TrangChuComponent, HeaderComponent, CarouselComponent, ContentTabComponent, FooterComponent, DetailMovieComponent, TimeOfMovieComponent],
+  declarations: [HomeComponent, TrangChuComponent, HeaderComponent, CarouselComponent, ContentTabComponent, FooterComponent, DetailMovieComponent, TimeOfMovieComponent, OrderTicketsComponent, DanhSachGheComponent, GheComponent],
   imports: [
     CommonModule,
     HomeRoutingModule, OwlModule, SharedModule
