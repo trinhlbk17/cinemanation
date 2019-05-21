b8390467afc816e13ca2aa35090c0f61f385ae29
tranquangkhai <tranquangkhaiiq@yahoo.com>
2019-05-21 18:16:13 +0700

    21/05/2019

--- src/app/home/trang-chu/detail-movie/time-of-movie/danh-sach-ghe/danh-sach-ghe.component.ts
+++ src/app/home/trang-chu/detail-movie/time-of-movie/danh-sach-ghe/danh-sach-ghe.component.ts
@@ -9,7 +9,46 @@ import { ActivatedRoute } from '@angular/router';
 export class DanhSachGheComponent implements OnInit {
   subParam: Subscription;
   subService: Subscription;
-  gio = 0;
+  thamsoArr: any = [];
+  maPhim: any;
+  gio: any;
+  danhsachghe=[
+    { SoGhe: 1, TenGhe: "số 1", Gia: 100, TrangThai: false },
+    { SoGhe: 2, TenGhe: "số 2", Gia: 100, TrangThai: false },
+    { SoGhe: 3, TenGhe: "số 3", Gia: 100, TrangThai: false },
+    { SoGhe: 4, TenGhe: "số 4", Gia: 100, TrangThai: false },
+    { SoGhe: 5, TenGhe: "số 5", Gia: 100, TrangThai: false },
+    { SoGhe: 6, TenGhe: "số 6", Gia: 100, TrangThai: false },
+    { SoGhe: 7, TenGhe: "số 7", Gia: 100, TrangThai: false },
+    { SoGhe: 8, TenGhe: "số 7", Gia: 100, TrangThai: false },
+    { SoGhe: 9, TenGhe: "số 9", Gia: 100, TrangThai: false },
+    { SoGhe: 10, TenGhe: "số 10", Gia: 100, TrangThai: false },
+    { SoGhe: 11, TenGhe: "số 11", Gia: 100, TrangThai: false },
+    { SoGhe: 12, TenGhe: "số 12", Gia: 100, TrangThai: false },
+    { SoGhe: 13, TenGhe: "số 13", Gia: 100, TrangThai: false },
+    { SoGhe: 14, TenGhe: "số 14", Gia: 100, TrangThai: false },
+    { SoGhe: 15, TenGhe: "số 15", Gia: 100, TrangThai: false },
+    { SoGhe: 16, TenGhe: "số 16", Gia: 100, TrangThai: false },
+    { SoGhe: 17, TenGhe: "số 17", Gia: 100, TrangThai: false },
+    { SoGhe: 18, TenGhe: "số 18", Gia: 100, TrangThai: false },
+    { SoGhe: 19, TenGhe: "số 19", Gia: 100, TrangThai: false },
+    { SoGhe: 20, TenGhe: "số 20", Gia: 100, TrangThai: false },
+    { SoGhe: 21, TenGhe: "số 21", Gia: 100, TrangThai: false },
+    { SoGhe: 22, TenGhe: "số 22", Gia: 100, TrangThai: false },
+    { SoGhe: 23, TenGhe: "số 23", Gia: 100, TrangThai: false },
+    { SoGhe: 24, TenGhe: "số 24", Gia: 100, TrangThai: false },
+    { SoGhe: 25, TenGhe: "số 25", Gia: 100, TrangThai: false },
+    { SoGhe: 26, TenGhe: "số 26", Gia: 100, TrangThai: false },
+    { SoGhe: 27, TenGhe: "số 27", Gia: 100, TrangThai: false },
+    { SoGhe: 28, TenGhe: "số 28", Gia: 100, TrangThai: false },
+    { SoGhe: 29, TenGhe: "số 29", Gia: 100, TrangThai: false },
+    { SoGhe: 30, TenGhe: "số 30", Gia: 100, TrangThai: true },
+    { SoGhe: 31, TenGhe: "số 31", Gia: 100, TrangThai: false },
+    { SoGhe: 32, TenGhe: "số 32", Gia: 100, TrangThai: false },
+    { SoGhe: 33, TenGhe: "số 33", Gia: 100, TrangThai: false },
+    { SoGhe: 34, TenGhe: "số 34", Gia: 100, TrangThai: false },
+    { SoGhe: 35, TenGhe: "số 35", Gia: 100, TrangThai: false }
+  ];
   constructor(
     private avtRoute: ActivatedRoute, // ActivatedRoute: là đối tượng dùng để lấy tham số từ URL (get params url)
   ) { }
@@ -17,8 +56,12 @@ export class DanhSachGheComponent implements OnInit {
   ngOnInit() {
     this.subParam =  this.avtRoute.params.subscribe((thamso: any) => {
       // Lấy tham số từ url thông qua thuộc tính params
-       this.gio = thamso.gio;
-       console.log(this.gio);
+       this.thamsoArr = thamso.gio.split(',');
+       this.maPhim = this.thamsoArr[0];
+      //  console.log(this.maPhim);
+       this.gio = this.thamsoArr[1];
+      //  console.log(this.gio);
+
   });
 
   }
--- src/app/home/trang-chu/detail-movie/time-of-movie/time-of-movie.component.ts
+++ src/app/home/trang-chu/detail-movie/time-of-movie/time-of-movie.component.ts
@@ -13,9 +13,9 @@ export class TimeOfMovieComponent implements OnInit {
   subParam: Subscription;
   subService: Subscription;
   maPhim: any;
-  gioo: any;
+  dsgioo: any = {};
+  gioo: number;
   phim: any = {};
-  lichchieu: any;
   constructor(
     private avtRoute: ActivatedRoute, // ActivatedRoute: là đối tượng dùng để lấy tham số từ URL (get params url)
     private qlyPhimService: QuanlyphimService
@@ -28,15 +28,24 @@ export class TimeOfMovieComponent implements OnInit {
     this.subParam =  this.avtRoute.params.subscribe((thamso: any) => {
       // Lấy tham số từ url thông qua thuộc tính params
     this.maPhim = thamso.maphim;
-    console.log("ma: "+ this.maPhim);
+    console.log(this.hour);
     this.LayChiTietPhim(this.maPhim);
+    // this.gioo = this.phim.LichChieu.NgayChieuGioChieu.getHours();
+    
   });
 }
 // Xây dựng phương thức lấy thông tin phim từ api
 LayChiTietPhim(maPhim: number) {
   this.subService = this.qlyPhimService.LayChiTietPhim(maPhim).subscribe((ketqua) => {
     this.phim = ketqua;
-    this.lichchieu = this.phim.LichChieu;
+    this.dsgioo = this.phim.LichChieu;
+    for(let malich of this.dsgioo) {
+      if(malich.MaLichChieu === this.ngay6) {
+        this.gioo = malich.NgayChieuGioChieu.substring(11, 13);
+        console.log("gio: " + this.gioo);
+        break;
+      }
+    }
   });
 }
   setanhien(gio): boolean {
